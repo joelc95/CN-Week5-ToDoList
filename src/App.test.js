1d589dbc3c7cca0964c602e15e06db5f971b5cfb
joelc95 <joelconalty@gmail.com>
2022-02-14 10:18:34 +0000

    updated test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders Header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/To Do List/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Renders Submit Button', () => {
+  render(<App />);
+  const element = screen.getByText(/^enter$/);
+  fireEvent.click(element)
+})
+
+test('Input element is rendered', () => {
+  render(<App />);
+  const input = screen.getByRole("textbox");
+  fireEvent.change(input, {target: {value: "gym"} });
+  expect(input.value).toBe("gym");
+})
